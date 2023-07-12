38c59e10540208f752b0178d3047470cc5cf3fb3
Robert Mayo <robert.mayo.ch@gmail.com>
2023-07-12 12:21:21 +0100

    food tests written and successful

--- tests/food_test.py
+++ tests/food_test.py
@@ -2,4 +2,15 @@ import unittest
 from src.food import Food
 
 class TestFood(unittest.TestCase):
-    pass
+    
+    def setUp(self):
+        self.food = Food("Pizza", 5, 25)
+
+    def test_food_has_name(self):
+        self.assertEqual("Pizza", self.food.name)
+
+    def test_food_has_price(self):
+        self.assertEqual(5, self.food.price)
+
+    def test_food_has_digestion_level(self):
+        self.assertEqual(25, self.food.digestion_level)
