99a12a4879496219e62b03ec9622b88cb942b68c
Robert Mayo <robert.mayo.ch@gmail.com>
2023-07-12 12:08:33 +0100

    defined classes

---
+++ src/customer.py
@@ -0,0 +1,8 @@
+class Customer:
+    def __init__(self, name, wallet, age, energy):
+        self.name = name
+        self.wallet = wallet
+        self.age = age
+        self.energy = energy
+
+    
---
+++ src/drink.py
@@ -0,0 +1,7 @@
+class Drink:
+    def __init__(self, name, price, caffiene_level):
+        self.name = name
+        self.price = price
+        self.caffiene_level = caffiene_level
+
+    
